358f9e11e9ecc7dcb4451bfd7a1fdc4df083b4bb
Peter McNeill <pemcneill8@gmail.com>
2015-04-29 22:45:50 -0400

    Basic functions are complete, supports search and copy to dest

--- librarian.py
+++ librarian.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
 """
 Usage:
-    librarian.py [options]
+    librarian.py --source=<source> --destination=<destination> [options]
 
 Options:
     --catalog=<catalog>     Catalog file [Default: catalog.yaml]
+    --dryrun                Don't actually do anything
     -h --help               Show help
 """
 
 from docopt import docopt
 import yaml
 import re
+import shutil
+import os
 
 # Globals
-catalog_file = None
-catalog = None
-# End
+DRYRUN = False
+# End globals
 
 
 def compare(name, catalog):
@@ -28,11 +30,11 @@ def compare(name, catalog):
                         return item['title']
             elif isinstance(item['regex'], str):
                 if re.match(item['regex'], name, re.IGNORECASE):
-                    return item['title']
+                    return item['target']
             else:
                 raise TypeError("Needs to be a string or an array")
         except:
-            raise KeyError("No index 'regex'")
+            raise KeyError("Malformed object in catalog: {id}".format(id=c))
     return None
 
 
@@ -40,22 +42,34 @@ def save(catalog):
     pass
 
 
-def load():
+def load(loadfile):
     try:
-        with open(catalog_file, 'r') as f:
+        with open(loadfile, 'r') as f:
             catalog = yaml.load(f)
         return catalog
     except:
         raise IOError("Error reading catalog file")
 
 
+def scanandcopy(dir, catalog, dest, scandirs=False):
+    for root, dirs, files in os.walk(dir):
+        for f in files:
+            if compare(f, catalog):
+                if DRYRUN:
+                    print("Matched {file} copying to {dest}".format(file=f, dest=os.path.join(dest, f)))
+                else:
+                    shutil.copy(os.path.join(root, f), os.path.join(dest, f))
+
 def main():
-    global catalog
-    catalog = load()
+    global DRYRUN
+    args = docopt(__doc__)
+    catalog_file = args['--catalog']
+    source_dir = args['--source']
+    dest_dir = args['--destination']
+    DRYRUN = args['--dryrun']
+    catalog = load(catalog_file)
+    scanandcopy(source_dir, catalog, dest_dir)
 
 
 if __name__ == "__main__":
-    global config
-    args = docopt(__doc__)
-    catalog_file = args['--catalog']
     main()
